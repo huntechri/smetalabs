82274b7cea803edf899cfa95de35509699a26668
huntechri <huntechri162378@gmail.com>
2026-05-18 18:33:59 +0300

    Global purchases: add supplier option types

--- types/global-purchases.ts
+++ types/global-purchases.ts
@@ -81,3 +81,13 @@ export type GlobalPurchaseMaterialOption = {
 export type GlobalPurchaseMaterialOptionsResponse = {
   data: GlobalPurchaseMaterialOption[]
 }
+
+export type GlobalPurchaseSupplierOption = {
+  id: string
+  name: string
+  color: string
+}
+
+export type GlobalPurchaseSupplierOptionsResponse = {
+  data: GlobalPurchaseSupplierOption[]
+}
